c3c40d2bde55f50e08db531f94da315753f55323
sashgorokhov <sashgorokhov@gmail.com>
2017-09-12 03:32:18 +0300

    Update readme with badges

--- README.rst
+++ README.rst
@@ -1,5 +1,14 @@
 scrapy_prometheus
-=================
+*****************
+
+.. image:: https://img.shields.io/pypi/status/python-telegram-handler.svg
+    :target: https://github.com/sashgorokhov/python-telegram-handler
+
+.. image:: https://travis-ci.org/sashgorokhov/scrapy_prometheus.svg?branch=master
+    :target: https://travis-ci.org/sashgorokhov/scrapy_prometheus
+
+.. image:: https://img.shields.io/github/license/sashgorokhov/python-telegram-handler.svg
+    :target: https://raw.githubusercontent.com/sashgorokhov/python-telegram-handler/master/LICENSE
 
 Exporting scrapy stats as prometheus metrics through pushgateway service.
 
--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ setup(
     name='scrapy_prometheus',
     version=VERSION,
     py_modules=['scrapy_prometheus'],
-    # TODO: Add url and download_url when github repo will be created
+    url='https://github.com/sashgorokhov/scrapy_prometheus',
+    download_url='https://github.com/sashgorokhov/scrapy_prometheus/archive/master.zip',
     keywords=['scrapy', 'prometheus', 'pushgateway', 'monitoring'],
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -19,7 +20,11 @@ setup(
         'Framework :: Scrapy',
         'License :: OSI Approved :: MIT License',
         'Topic :: System :: Monitoring',
-        'Programming Language :: Python :: 3.5',  # TODO: Add more when tests added
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     long_description=long_description,
     license='MIT License',
