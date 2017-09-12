26e158f86862f76efb99d4b70b2264b52776e953
sashgorokhov <sashgorokhov@gmail.com>
2017-09-12 03:42:57 +0300

    Initial release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 with open('README.rst') as readme:
     long_description = readme.read()
 
-VERSION = '2.0.1'
+VERSION = '0.1'
 
 setup(
     install_requires=['prometheus_client'],
